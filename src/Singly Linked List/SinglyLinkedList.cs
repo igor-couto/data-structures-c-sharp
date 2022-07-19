376d3ad38d957644a3dcc8e954f6822334fa228f
Igor Couto <igor.fcouto@gmail.com>
2022-07-19 00:58:37 -0300

    Add SinglyLinkedList

--- src/Singly Linked List/SinglyLinkedList.cs
+++ src/Singly Linked List/SinglyLinkedList.cs
@@ -5,30 +5,112 @@ public class SinglyLinkedList
     private Node _firstNode;
     private Node _lastNode;
 
+    public int Count { get; private set; }
+
     // Θ(1)
-    public void Insert(int key, int value)
+    public Node InsertFirst(int value)
     {
-        var newNode = new Node(key, value);
+        var newNode = new Node(value);
+        newNode.Next = _firstNode;
+
+        _firstNode = newNode;
 
         if (_lastNode is null)
-        {
             _lastNode = newNode;
-            _firstNode = _lastNode;
-        }
+
+        Count++;
+        return newNode;
+    }
+
+    // Θ(1)
+    public void InsertFirst(Node newNode)
+    {
+        newNode.Next = _firstNode;
+
+        _firstNode = newNode;
+
+        if (_lastNode is null)
+            _lastNode = newNode;
+
+        Count++;
+    }
+
+    // Θ(1)
+    public Node InsertLast(int value)
+    {
+        var newNode = new Node(value);
+
+        if (_lastNode is null)
+            _firstNode = newNode;
         else
-        {
             _lastNode.Next = newNode;
-            _lastNode = newNode;
-        }
+
+        _lastNode = newNode;
+
+        Count++;
+        return newNode;
     }
 
-    // Θ(n)
-    public int? Access(int key)
+    // Θ(1)
+    public void InsertLast(Node newNode)
+    {
+        if (_lastNode is null)
+            _firstNode = newNode;
+        else
+            _lastNode.Next = newNode;
+
+        _lastNode = newNode;
+
+        Count++;
+    }
+
+    // Θ(1)
+    public void InsertAfter(Node node, int value)
+    {
+        var newNode = new Node(value);
+        InsertAfter(node, newNode);
+    }
+
+    // Θ(1)
+    public void InsertAfter(Node node, Node newNode)
+    {
+        newNode.Next = node.Next;
+        node.Next = newNode;
+
+        Count++;
+    }
+
+    // Θ(1)
+    public void InsertBefore(Node node, Node newNode)
     {
         var currentNode = _firstNode;
         while (currentNode is not null)
         {
-            if (currentNode.Key == key) return currentNode.Value;
+            if (currentNode.Next == node)
+                currentNode.Next = newNode;
+
+            currentNode = currentNode.Next;
+        }
+
+        newNode.Next = node;
+        Count++;
+    }
+
+    // Θ(1)
+    public void InsertBefore(Node node, int value)
+    {
+        var newNode = new Node(value);
+        InsertBefore(node, newNode);
+    }
+
+    // Θ(n)
+    public Node Access(int value)
+    {
+        var currentNode = _firstNode;
+        while (currentNode is not null)
+        {
+            if (currentNode.Value == value)
+                return currentNode;
 
             currentNode = currentNode.Next;
         }
@@ -36,12 +118,42 @@ public class SinglyLinkedList
     }
 
     // Θ(n)
-    public int? Search(int value)
+    public Node AccessLast(int value)
+    {
+        Node lastNode = null;
+
+        var currentNode = _firstNode;
+        while (currentNode is not null)
+        {
+            if (currentNode.Value == value)
+                lastNode = currentNode;
+
+            currentNode = currentNode.Next;
+        }
+        return lastNode;
+    }
+
+    // Θ(n)
+    public bool Contains(int value)
     {
         var currentNode = _firstNode;
         while (currentNode is not null)
         {
-            if (currentNode.Value == value) return currentNode.Key;
+            if (currentNode.Value == value)
+                return true;
+
+            currentNode = currentNode.Next;
+        }
+        return false;
+    }
+
+    // Θ(n)
+    public Node Search(int value)
+    {
+        var currentNode = _firstNode;
+        while (currentNode is not null)
+        {
+            if (currentNode.Value == value) return currentNode;
 
             currentNode = currentNode.Next;
         }
@@ -49,13 +161,13 @@ public class SinglyLinkedList
     }
 
     // Θ(n)
-    public void Delete(int key)
+    public void Delete(Node node)
     {
         var previousNode = _firstNode;
         var currentNode = _firstNode;
         while (currentNode is not null)
         {
-            if (currentNode.Key == key)
+            if (currentNode == node)
             {
                 previousNode.Next = currentNode.Next;
 
@@ -68,9 +180,38 @@ public class SinglyLinkedList
                 currentNode = null;
                 break;
             }
-            previousNode = currentNode;
-            currentNode = currentNode.Next;
         }
+        Count--;
+    }
+
+    // Θ(n)
+    public void Delete(int value)
+    {
+        var node = new Node(value);
+        Delete(node);
+    }
+
+    // Θ(n)
+    public void DeleteFirst()
+        => Delete(_firstNode);
+
+    // Θ(n)
+    public void DeleteLast()
+        => Delete(_lastNode);
+
+    public void Clear()
+    {
+        var currentNode = _firstNode;
+        while (currentNode is not null)
+        {
+            var next = currentNode.Next;
+            currentNode.Next = null;
+            currentNode = next;
+        }
+
+        _firstNode = null;
+        _lastNode = null;
+        Count = 0;
     }
 
     public void Print()
@@ -78,22 +219,23 @@ public class SinglyLinkedList
         var currentNode = _firstNode;
         while (currentNode is not null)
         {
-            Console.WriteLine($"[Key: {currentNode.Key}, Value:{currentNode.Value}]");
+            unsafe
+            {
+                var o = new object();
+                TypedReference tr = __makeref(o);
+                IntPtr ptr = **(IntPtr**)(&tr);
+
+                Console.WriteLine($"[Node: {ptr}, Value:{currentNode.Value}]");
+            }
             currentNode = currentNode.Next;
         }
-        Console.WriteLine();
     }
 }
 
 public class Node
 {
-    public int Key { get; set; }
     public int Value { get; set; }
     public Node Next { get; set; }
 
-    public Node(int key, int value)
-    {
-        Key = key;
-        Value = value;
-    }
+    public Node(int value) => Value = value;
 }
