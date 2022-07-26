6339dfccdcc01a9c6ce5719516560bfd3fc7216f
Igor Couto <igor.fcouto@gmail.com>
2022-07-25 23:21:22 -0300

    Add DoublyLinkedList node

---
+++ src/DoublyLinkedList/Node.cs
@@ -0,0 +1,22 @@
+namespace DataStructures.DoublyLinkedList;
+
+public class Node
+{
+    public int Value { get; set; }
+    public Node Previous { get; set; }
+    public Node Next { get; set; }
+
+    public Node(int value) => Value = value;
+
+    public override string ToString()
+    {
+        unsafe
+        {
+            var o = new object();
+            TypedReference tr = __makeref(o);
+            IntPtr ptr = **(IntPtr**)(&tr);
+
+            return $"[Node: {ptr}, Value:{Value} - Next: {Next.Value} - Previous: {Previous.Value}]";
+        }
+    }
+}
